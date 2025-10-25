1194ad990c1f791bb89fcc44a802d00b71ccadc5
afsalboot <afsalr71@gmail.com>
2025-10-26 01:15:00 +0530

    Last Commit

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -1,13 +1,4 @@
-// Nodemailer Config
-// const nodemailer = require('nodemailer');
 
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS,
-//     },
-// });
 const Resend = require('resend')
 
 const resend = new Resend(process.env.RESEND_API_KEY)
