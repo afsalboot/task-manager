5ef671d275fc941097583e58b87fb093aeb7830d
afsalboot <afsalr71@gmail.com>
2025-10-26 10:38:01 +0530

    Last Commit

--- server/routes/cronRoutes.js
+++ server/routes/cronRoutes.js
@@ -5,12 +5,16 @@ const checkTasks = require("../corn/taskNotifier");
 
 router.get("/run-tasks", async (req, res) => {
   try {
+    const key = req.query.key;
+    if (key !== process.env.CRON_KEY) {
+      return res.status(403).send("Unauthorized");
+    }
+
     await checkTasks();
     res.status(200).send("Task check completed successfully");
   } catch (error) {
-    console.error(error);
+    console.error("Error running task check:", error);
     res.status(500).send("Error running task check");
   }
 });
 
-module.exports = router;
